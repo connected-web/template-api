9562d4cea570259817b118c9af8ab66820a059d0
John Beech <johnbeech.github@mkv25.net>
2023-09-05 22:27:50 +0100

    Update post-deployment tests

--- api/src/tests/post-deployment/api-client.d.ts
+++ api/src/tests/post-deployment/api-client.d.ts
@@ -16,60 +16,6 @@ declare namespace Components {
       [name: string]: any
     }
     /**
-         * Data Item Deleted
-         */
-    export interface DataItemDeletedModel {
-      message: string
-    }
-    /**
-         * List Data Items
-         */
-    export interface DataItemListModel {
-      items: /* Data Item */ DataItemModel[]
-    }
-    /**
-         * Data Item
-         */
-    export interface DataItemModel {
-      [name: string]: any
-    }
-    /**
-         * Data Item Not Found
-         */
-    export interface DataItemNotFoundModel {
-      message: string
-    }
-    /**
-         * Data Item Stored
-         */
-    export interface DataItemStoredModel {
-      message: string
-    }
-    /**
-         * Schema Deleted
-         */
-    export interface SchemaDeletedModel {
-      message: string
-    }
-    /**
-         * List Schemas
-         */
-    export interface SchemaListModel {
-      schemaIds?: string[]
-    }
-    /**
-         * Schema Not Found
-         */
-    export interface SchemaNotFoundModel {
-      message: string
-    }
-    /**
-         * Schema Stored
-         */
-    export interface SchemaStoredModel {
-      message: string
-    }
-    /**
          * Status
          */
     export interface StatusResponseModel {
@@ -78,76 +24,9 @@ declare namespace Components {
              */
       deploymentTime: string
     }
-    /**
-         * Stored Schema
-         */
-    export interface StorableSchemaModel {
-      $schema: string
-      title: string
-    }
   }
 }
 declare namespace Paths {
-  namespace Data$SchemaId {
-    namespace Options {
-      namespace Parameters {
-        export type SchemaId = string
-      }
-      export interface PathParameters {
-        schemaId: Parameters.SchemaId
-      }
-    }
-  }
-  namespace Data$SchemaId$ItemId {
-    namespace Options {
-      namespace Parameters {
-        export type ItemId = string
-        export type SchemaId = string
-      }
-      export interface PathParameters {
-        schemaId: Parameters.SchemaId
-        itemId: Parameters.ItemId
-      }
-    }
-  }
-  namespace DeleteData {
-    namespace Parameters {
-      export type ItemId = string
-      export type SchemaId = string
-    }
-    export interface PathParameters {
-      schemaId: Parameters.SchemaId
-      itemId: Parameters.ItemId
-    }
-    namespace Responses {
-      export type $200 = /* Data Item Deleted */ Components.Schemas.DataItemDeletedModel
-    }
-  }
-  namespace DeleteSchema {
-    namespace Parameters {
-      export type SchemaId = string
-    }
-    export interface PathParameters {
-      schemaId: Parameters.SchemaId
-    }
-    namespace Responses {
-      export type $200 = /* Schema Deleted */ Components.Schemas.SchemaDeletedModel
-    }
-  }
-  namespace GetData {
-    namespace Parameters {
-      export type ItemId = string
-      export type SchemaId = string
-    }
-    export interface PathParameters {
-      schemaId: Parameters.SchemaId
-      itemId: Parameters.ItemId
-    }
-    namespace Responses {
-      export type $200 = /* Data Item */ Components.Schemas.DataItemModel
-      export type $404 = /* Data Item Not Found */ Components.Schemas.DataItemNotFoundModel
-    }
-  }
   namespace GetOpenAPISpec {
     namespace Responses {
       export type $200 = /**
@@ -157,112 +36,14 @@ declare namespace Paths {
             Components.Schemas.BasicObjectModel
     }
   }
-  namespace GetSchema {
-    namespace Parameters {
-      export type SchemaId = string
-    }
-    export interface PathParameters {
-      schemaId: Parameters.SchemaId
-    }
-    namespace Responses {
-      export type $200 = /* Stored Schema */ Components.Schemas.StorableSchemaModel
-      export type $404 = /* Schema Not Found */ Components.Schemas.SchemaNotFoundModel
-    }
-  }
   namespace GetStatus {
     namespace Responses {
       export type $200 = /* Status */ Components.Schemas.StatusResponseModel
     }
   }
-  namespace Items$SchemaId {
-    namespace Options {
-      namespace Parameters {
-        export type SchemaId = string
-      }
-      export interface PathParameters {
-        schemaId: Parameters.SchemaId
-      }
-    }
-  }
-  namespace ListItems {
-    namespace Parameters {
-      export type SchemaId = string
-    }
-    export interface PathParameters {
-      schemaId: Parameters.SchemaId
-    }
-    namespace Responses {
-      export type $200 = /* List Data Items */ Components.Schemas.DataItemListModel
-    }
-  }
-  namespace ListSchemas {
-    namespace Responses {
-      export type $200 = /* List Schemas */ Components.Schemas.SchemaListModel
-    }
-  }
-  namespace PutData {
-    namespace Parameters {
-      export type ItemId = string
-      export type SchemaId = string
-    }
-    export interface PathParameters {
-      schemaId: Parameters.SchemaId
-      itemId: Parameters.ItemId
-    }
-    export type RequestBody = /* Data Item */ Components.Schemas.DataItemModel
-    namespace Responses {
-      export type $200 = /* Data Item Stored */ Components.Schemas.DataItemStoredModel
-    }
-  }
-  namespace PutSchema {
-    namespace Parameters {
-      export type SchemaId = string
-    }
-    export interface PathParameters {
-      schemaId: Parameters.SchemaId
-    }
-    export type RequestBody = /* Stored Schema */ Components.Schemas.StorableSchemaModel
-    namespace Responses {
-      export type $200 = /* Schema Stored */ Components.Schemas.SchemaStoredModel
-    }
-  }
-  namespace Schema$SchemaId {
-    namespace Options {
-      namespace Parameters {
-        export type SchemaId = string
-      }
-      export interface PathParameters {
-        schemaId: Parameters.SchemaId
-      }
-    }
-  }
 }
 
 export interface OperationMethods {
-  /**
-   * getSchema
-   */
-  'getSchema': (
-    parameters?: Parameters<Paths.GetSchema.PathParameters> | null,
-    data?: any,
-    config?: AxiosRequestConfig
-  ) => OperationResponse<Paths.GetSchema.Responses.$200>
-  /**
-   * putSchema
-   */
-  'putSchema': (
-    parameters?: Parameters<Paths.PutSchema.PathParameters> | null,
-    data?: Paths.PutSchema.RequestBody,
-    config?: AxiosRequestConfig
-  ) => OperationResponse<Paths.PutSchema.Responses.$200>
-  /**
-   * deleteSchema
-   */
-  'deleteSchema': (
-    parameters?: Parameters<Paths.DeleteSchema.PathParameters> | null,
-    data?: any,
-    config?: AxiosRequestConfig
-  ) => OperationResponse<Paths.DeleteSchema.Responses.$200>
   /**
    * getStatus
    */
@@ -272,38 +53,6 @@ export interface OperationMethods {
     config?: AxiosRequestConfig
   ) => OperationResponse<Paths.GetStatus.Responses.$200>
   /**
-   * getData
-   */
-  'getData': (
-    parameters?: Parameters<Paths.GetData.PathParameters> | null,
-    data?: any,
-    config?: AxiosRequestConfig
-  ) => OperationResponse<Paths.GetData.Responses.$200>
-  /**
-   * putData
-   */
-  'putData': (
-    parameters?: Parameters<Paths.PutData.PathParameters> | null,
-    data?: Paths.PutData.RequestBody,
-    config?: AxiosRequestConfig
-  ) => OperationResponse<Paths.PutData.Responses.$200>
-  /**
-   * deleteData
-   */
-  'deleteData': (
-    parameters?: Parameters<Paths.DeleteData.PathParameters> | null,
-    data?: any,
-    config?: AxiosRequestConfig
-  ) => OperationResponse<Paths.DeleteData.Responses.$200>
-  /**
-   * listItems
-   */
-  'listItems': (
-    parameters?: Parameters<Paths.ListItems.PathParameters> | null,
-    data?: any,
-    config?: AxiosRequestConfig
-  ) => OperationResponse<Paths.ListItems.Responses.$200>
-  /**
    * getOpenAPISpec
    */
   'getOpenAPISpec': (
@@ -311,47 +60,9 @@ export interface OperationMethods {
     data?: any,
     config?: AxiosRequestConfig
   ) => OperationResponse<Paths.GetOpenAPISpec.Responses.$200>
-  /**
-   * listSchemas
-   */
-  'listSchemas': (
-    parameters?: Parameters<UnknownParamsObject> | null,
-    data?: any,
-    config?: AxiosRequestConfig
-  ) => OperationResponse<Paths.ListSchemas.Responses.$200>
 }
 
 export interface PathsDictionary {
-  ['/items']: {
-  }
-  ['/data/{schemaId}']: {
-  }
-  ['/schema/{schemaId}']: {
-    /**
-     * getSchema
-     */
-    'get': (
-      parameters?: Parameters<Paths.GetSchema.PathParameters> | null,
-      data?: any,
-      config?: AxiosRequestConfig
-    ) => OperationResponse<Paths.GetSchema.Responses.$200>
-    /**
-     * putSchema
-     */
-    'put': (
-      parameters?: Parameters<Paths.PutSchema.PathParameters> | null,
-      data?: Paths.PutSchema.RequestBody,
-      config?: AxiosRequestConfig
-    ) => OperationResponse<Paths.PutSchema.Responses.$200>
-    /**
-     * deleteSchema
-     */
-    'delete': (
-      parameters?: Parameters<Paths.DeleteSchema.PathParameters> | null,
-      data?: any,
-      config?: AxiosRequestConfig
-    ) => OperationResponse<Paths.DeleteSchema.Responses.$200>
-  }
   ['/status']: {
     /**
      * getStatus
@@ -361,46 +72,6 @@ export interface PathsDictionary {
       data?: any,
       config?: AxiosRequestConfig
     ) => OperationResponse<Paths.GetStatus.Responses.$200>
-  }
-  ['/data/{schemaId}/{itemId}']: {
-    /**
-     * getData
-     */
-    'get': (
-      parameters?: Parameters<Paths.GetData.PathParameters> | null,
-      data?: any,
-      config?: AxiosRequestConfig
-    ) => OperationResponse<Paths.GetData.Responses.$200>
-    /**
-     * putData
-     */
-    'put': (
-      parameters?: Parameters<Paths.PutData.PathParameters> | null,
-      data?: Paths.PutData.RequestBody,
-      config?: AxiosRequestConfig
-    ) => OperationResponse<Paths.PutData.Responses.$200>
-    /**
-     * deleteData
-     */
-    'delete': (
-      parameters?: Parameters<Paths.DeleteData.PathParameters> | null,
-      data?: any,
-      config?: AxiosRequestConfig
-    ) => OperationResponse<Paths.DeleteData.Responses.$200>
-  }
-  ['/schema']: {
-  }
-  ['/items/{schemaId}']: {
-    /**
-     * listItems
-     */
-    'get': (
-      parameters?: Parameters<Paths.ListItems.PathParameters> | null,
-      data?: any,
-      config?: AxiosRequestConfig
-    ) => OperationResponse<Paths.ListItems.Responses.$200>
-  }
-  ['/data']: {
   }
   ['/openapi']: {
     /**
@@ -413,16 +84,6 @@ export interface PathsDictionary {
     ) => OperationResponse<Paths.GetOpenAPISpec.Responses.$200>
   }
   ['/']: {
-  }
-  ['/schemas']: {
-    /**
-     * listSchemas
-     */
-    'get': (
-      parameters?: Parameters<UnknownParamsObject> | null,
-      data?: any,
-      config?: AxiosRequestConfig
-    ) => OperationResponse<Paths.ListSchemas.Responses.$200>
   }
 }
 
--- api/src/tests/post-deployment/api-client.test.ts
+++ api/src/tests/post-deployment/api-client.test.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it, beforeAll, afterAll } from '@jest/globals'
+import { expect, describe, it, beforeAll } from '@jest/globals'
 
 import axios from 'axios'
 import OpenAPIClientAxios, { Document } from 'openapi-client-axios'
@@ -117,17 +117,7 @@ describe('Open API Spec', () => {
       const pathStrings = Object.keys(openapiDoc.paths as any).sort()
       expect(pathStrings).toEqual([
         '/',
-        '/data',
-        '/data/{schemaId}',
-        '/data/{schemaId}/{itemId}',
-        '/data/{schemaId}/{itemId}/versions',
-        '/data/{schemaId}/{itemId}/versions/{versionId}',
-        '/items',
-        '/items/{schemaId}',
         '/openapi',
-        '/schema',
-        '/schema/{schemaId}',
-        '/schemas',
         '/status'
       ])
     })
@@ -141,11 +131,7 @@ describe('Open API Spec', () => {
       expect(actualClientKeys).toEqual(
         expect.arrayContaining([
           'getOpenAPISpec',
-          'getStatus',
-          'listSchemas',
-          'getSchema',
-          'putSchema',
-          'deleteSchema'
+          'getStatus'
         ])
       )
     })
@@ -186,209 +172,6 @@ describe('Open API Spec', () => {
 
       ajv.validate({ $ref: 'app-openapi.json#/components/schemas/BasicObjectModel' }, response.data)
       expect(ajv.errors ?? []).toEqual([])
-    })
-  })
-
-  describe('Schema Endpoints', () => {
-    let appClient: ApiClientUnderTest
-    beforeAll(async () => {
-      const axiosApi = new OpenAPIClientAxios({
-        definition: openapiDoc,
-        axiosConfigDefaults: {
-          headers: serverConfig.headers,
-          validateStatus: function (status) {
-            return status >= 200 // don't throw errors on non-200 codes
-          }
-        }
-      })
-
-      appClient = await axiosApi.getClient<ApiClientUnderTest>()
-      appClient.interceptors.response.use((response) => response, (error) => {
-        console.log('Caught client error:', error.message)
-      })
-    })
-
-    const testSchema = { $schema: 'https://json-schema.org/draft/2020-12/schema', title: 'Post Deployment Test Schema' }
-
-    it('should be possible to create or update a schema', async () => {
-      const response = await appClient.putSchema({ schemaId: 'post-deployment-test-id' }, testSchema)
-
-      console.log('Put Schema:', response.status, response.statusText, JSON.stringify(response.data, null, 2))
-
-      ajv.validate({ $ref: 'app-openapi.json#/components/schemas/SchemaStoredModel' }, response.data)
-      expect(ajv.errors ?? []).toEqual([])
-      expect(response.data.message).toEqual(`PUT /schema/post-deployment-test-id success (${JSON.stringify(testSchema).length} bytes)`)
-    })
-
-    it('should handle schema compilation error', async () => {
-      // Simulate a schema compilation error
-      const invalidSchema = { type: 'invalid_type' }
-      const response = await appClient.putSchema({ schemaId: 'invalid-schema' }, invalidSchema as any)
-
-      expect(response.status).toEqual(400)
-      expect(response.data).toHaveProperty('message', 'Unable to compile schema: schema is invalid: data/type must be equal to one of the allowed values, data/type must be array, data/type must match a schema in anyOf')
-    })
-
-    it('should be possible to get an existing schema', async () => {
-      const response = await appClient.getSchema({ schemaId: 'post-deployment-test-id' })
-
-      console.log('Get Schema (existing):', response.status, response.statusText, JSON.stringify(response.data, null, 2))
-
-      ajv.validate({ $ref: 'app-openapi.json#/components/schemas/StorableSchemaModel' }, response.data)
-      expect(ajv.errors ?? []).toEqual([])
-      expect(response.data).toEqual(testSchema)
-    })
-
-    it('should be possible to get a non-existing schema', async () => {
-      const response = await appClient.getSchema({ schemaId: 'post-deployment-test-non-existing-id' })
-
-      console.log('Get Schema (non-existing):', response.status, response.statusText, JSON.stringify(response.data, null, 2))
-
-      ajv.validate({ $ref: 'app-openapi.json#/components/schemas/SchemaNotFoundModel' }, response.data)
-      expect(ajv.errors ?? []).toEqual([])
-      expect((response.data as any).message).toEqual('Unable to find schema with post-deployment-test-non-existing-id. Error: The specified key does not exist.')
-    })
-
-    it('should be possible to list available schemas', async () => {
-      const response = await appClient.listSchemas()
-
-      console.log('List Schemas:', response.status, response.statusText, JSON.stringify(response.data, null, 2))
-
-      ajv.validate({ $ref: 'app-openapi.json#/components/schemas/SchemaListModel' }, response.data)
-      expect(ajv.errors ?? []).toEqual([])
-      expect(response.data).toEqual({ schemaIds: ['post-deployment-test-id'] })
-    })
-
-    it('should be possible to delete a schema', async () => {
-      const response = await appClient.deleteSchema({ schemaId: 'post-deployment-test-id' })
-
-      console.log('Delete Schema:', response.status, response.statusText, JSON.stringify(response.data, null, 2))
-
-      ajv.validate({ $ref: 'app-openapi.json#/components/schemas/SchemaDeletedModel' }, response.data)
-      expect(ajv.errors ?? []).toEqual([])
-      expect(response.data.message).toEqual('DELETE /schema/post-deployment-test-id OK')
-    })
-  })
-
-  describe('Data Item Endpoints', () => {
-    let appClient: ApiClientUnderTest
-    const testSchema = {
-      $schema: 'https://json-schema.org/draft/2020-12/schema',
-      title: 'Post Deployment Test Schema',
-      properties: {
-        name: {
-          type: 'string'
-        },
-        timestamp: {
-          type: 'string',
-          format: 'date-time'
-        }
-      },
-      required: ['name', 'timestamp'],
-      allowedAdditionalProperties: false
-    }
-    const testSchemaId = 'post-deployment-data-item-test-id'
-    const testDataItem = { name: 'Test Item (xyz-123)', timestamp: (new Date()).toISOString() }
-
-    beforeAll(async () => {
-      const axiosApi = new OpenAPIClientAxios({
-        definition: openapiDoc,
-        axiosConfigDefaults: {
-          headers: serverConfig.headers,
-          validateStatus: function (status) {
-            return status >= 200 // don't throw errors on non-200 codes
-          }
-        }
-      })
-
-      appClient = await axiosApi.getClient<ApiClientUnderTest>()
-      appClient.interceptors.response.use((response) => response, (error) => {
-        console.log('Caught client error:', error.message)
-      })
-
-      const putSchemaResponse = await appClient.putSchema({ schemaId: testSchemaId }, testSchema)
-      console.log('Put Data Item Schema:', putSchemaResponse.status, putSchemaResponse.statusText, JSON.stringify(putSchemaResponse.data, null, 2))
-    })
-
-    afterAll(async () => {
-      const deleteSchemaResponse = await appClient.deleteSchema({ schemaId: testSchemaId }, testSchema)
-      console.log('Delete Data Item Schema:', deleteSchemaResponse.status, deleteSchemaResponse.statusText, JSON.stringify(deleteSchemaResponse.data, null, 2))
-    })
-
-    it('should be possible to create or update a data item against an existing schema', async () => {
-      const testItemId = 'xyz-123'
-      const response = await appClient.putData({ schemaId: testSchemaId, itemId: testItemId }, testDataItem)
-
-      console.log('Put Data Item:', response.status, response.statusText, JSON.stringify(response.data, null, 2))
-
-      ajv.validate({ $ref: 'app-openapi.json#/components/schemas/DataItemModel' }, response.data)
-      expect(ajv.errors ?? []).toEqual([])
-      expect(response.data.message).toEqual('PUT /data/post-deployment-data-item-test-id/xyz-123 success')
-    })
-
-    it('should handle data validation error', async () => {
-      try {
-        await appClient.deleteData({ schemaId: testSchemaId, itemId: 'invalid-data' })
-      } catch (ex) {
-        const error = ex as Error
-        console.error('Caught error:', error.message, 'while deleting invalid data item')
-      }
-      // Simulate a data validation error
-      const invalidData = { name: 123, timestamp: (new Date()).toISOString() } // Name should be a string
-      const response = await appClient.putData({ schemaId: testSchemaId, itemId: 'invalid-data' }, invalidData)
-
-      expect(response.status).toEqual(400)
-      expect(response.data).toHaveProperty('message', 'Unable to validate data: [{"instancePath":"/name","schemaPath":"#/properties/name/type","keyword":"type","params":{"type":"string"},"message":"must be string"}]')
-    })
-
-    it('should be possible to get an existing stored data item', async () => {
-      const response = await appClient.getData({ schemaId: testSchemaId, itemId: 'xyz-123' })
-
-      console.log('Get Data Item (existing):', response.status, response.statusText, JSON.stringify(response.data, null, 2))
-
-      ajv.validate({ $ref: 'app-openapi.json#/components/schemas/DataItemModel' }, response.data)
-      expect(ajv.errors ?? []).toEqual([])
-      expect(response.data).toEqual(testDataItem)
-    })
-
-    it('should be possible to get a non-existing data item - based on non-existing schema id', async () => {
-      const response = await appClient.getData({ schemaId: 'non-existing-schema-id', itemId: 'xyz-123' })
-
-      console.log('Get Data Item (non-existing schema id):', response.status, response.statusText, JSON.stringify(response.data, null, 2))
-
-      ajv.validate({ $ref: 'app-openapi.json#/components/schemas/DataItemNotFoundModel' }, response.data)
-      expect(ajv.errors ?? []).toEqual([])
-      expect((response.data as any).message).toEqual('Unable to find data with schemaId: non-existing-schema-id and itemId: xyz-123. Error: The specified key does not exist.')
-    })
-
-    it('should be possible to get a non-existing data item - based on non-existing item id', async () => {
-      const response = await appClient.getData({ schemaId: testSchemaId, itemId: 'non-existing' })
-
-      console.log('Get Data Item (non-existing item id):', response.status, response.statusText, JSON.stringify(response.data, null, 2))
-
-      ajv.validate({ $ref: 'app-openapi.json#/components/schemas/DataItemNotFoundModel' }, response.data)
-      expect(ajv.errors ?? []).toEqual([])
-      expect((response.data as any).message).toEqual('Unable to find data with schemaId: post-deployment-data-item-test-id and itemId: non-existing. Error: The specified key does not exist.')
-    })
-
-    it('should be possible to list available data items associated with a schema id', async () => {
-      const response = await appClient.listItems({ schemaId: testSchemaId })
-
-      console.log(`List Data Items: ${testSchemaId}`, response.status, response.statusText, JSON.stringify(response.data, null, 2))
-
-      ajv.validate({ $ref: 'app-openapi.json#/components/schemas/DataItemListModel' }, response.data)
-      expect(ajv.errors ?? []).toEqual([])
-      expect(response.data).toEqual({ itemIds: ['xyz-123'] })
-    })
-
-    it('should be possible to delete a data item', async () => {
-      const response = await appClient.deleteData({ schemaId: testSchemaId, itemId: 'xyz-123' })
-
-      console.log('Delete Schema:', response.status, response.statusText, JSON.stringify(response.data, null, 2))
-
-      ajv.validate({ $ref: 'app-openapi.json#/components/schemas/SchemaDeletedModel' }, response.data)
-      expect(ajv.errors ?? []).toEqual([])
-      expect(response.data.message).toEqual('DELETE /data/post-deployment-data-item-test-id/xyz-123 OK')
     })
   })
 })
